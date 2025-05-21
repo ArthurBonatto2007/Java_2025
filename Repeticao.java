9c312cf7951be1bd5c0b46a5625ee7f1ec1f9ac9
ArthurBonatto2007 <arthurbonatto187@gmail.com>
2025-05-21 11:30:57 -0300

    Add files via upload

---
+++ Repeticao.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticao;
+
+/**
+ *
+ * @author ARTHURBONATTODALLAGN
+ */
+public class Repeticao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("escreva do 1 ao 0 == ");
+                for(int i=1;i<=10;i++){
+                    if(i%2 == 0){
+                    System.out.println(i);
+    }
+                }
+}}
---
+++ Tabuada.java
@@ -0,0 +1,27 @@
+package tabuada;
+
+import java.util.Scanner;
+
+public class Tabuada {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int n = sc.nextInt();
+
+        System.out.println("Tabuada do " + n + ":");
+        System.out.println(n + " x 1 = " + (n * 1));
+        System.out.println(n + " x 2 = " + (n * 2));
+        System.out.println(n + " x 3 = " + (n * 3));
+        System.out.println(n + " x 4 = " + (n * 4));
+        System.out.println(n + " x 5 = " + (n * 5));
+        System.out.println(n + " x 6 = " + (n * 6));
+        System.out.println(n + " x 7 = " + (n * 7));
+        System.out.println(n + " x 8 = " + (n * 8));
+        System.out.println(n + " x 9 = " + (n * 9));
+        System.out.println(n + " x 10 = " + (n * 10));
+
+        sc.close();
+    }
+}
